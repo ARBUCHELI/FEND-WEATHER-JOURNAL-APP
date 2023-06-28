75722d16ed7b2c33cfd79cf207d49110778c4745
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-28 11:36:15 +0300

    creating global variables in website/app.js

--- website/app.js
+++ website/app.js
@@ -1,5 +1,10 @@
 /* Global Variables */
+// Global Variables
+const apiKey = 'adb41019c34f749ea04e98d59b8c7561';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
