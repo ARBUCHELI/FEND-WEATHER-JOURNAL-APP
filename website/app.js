d1e6ba681b39054a4a740478fb95f537e744644d
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-28 11:37:28 +0300

    creating an async function to make a get request to the api

--- website/app.js
+++ website/app.js
@@ -8,3 +8,17 @@ const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+// Async function to get weather data from OpenWeatherMap API
+async function getWeatherData(zipCode) {
+    const apiUrl = `${baseUrl}?zip=${zipCode}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+  
+    try {
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log('Error:', error);
+      return null;
+    }
+  }
+  
