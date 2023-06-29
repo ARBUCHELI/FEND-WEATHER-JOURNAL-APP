67ce09522e28043a7300939375cf209de74a7c6e
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-29 08:07:17 +0300

    feat: Rubric Documentation -Adding comments

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 // Setup empty JS object to act as endpoint for all routes
+/**
+ * Project Rubric Success Criteria - APP API Endpoint: There should be a JavaScript Object named projectData initiated in the 
+ * file server.js to act as the app API endpoint.
+ */
 let projectData = {};
 
 // Require Express to run server and routes
 /**
- * Project Rubric Success Criteria - Node and Express Environment: Node and Express should be installed on the local machine. 
- * The project file server.js should require express(), and should create an instance of their app using express.
+ * Project Rubric PROJECT ENVIRONMENT SETUP. Success Criteria - Node and Express Environment: Node and Express should be 
+ * installed on the local machine. The project file server.js should require express(), and should create an instance of their 
+ * app using express.
  */
 const express = require('express');
+
 /**
- * Project Rubric Success Criteria - Project Dependencies: The ‘cors’ package should be installed in the project from the command
- * line, required in the project file server.js, and the instance of the app should be setup to use cors().
+ * Project Rubric PROJECT ENVIRONMENT SETUP. Success Criteria - Project Dependencies: The ‘cors’ package should be installed in 
+ * the project from the command line, required in the project file server.js, and the instance of the app should be setup to use 
+ * cors().
  */
 const cors = require('cors');
+
 /**
- * Project Rubric Success Criteria - Project Dependencies: The body-parser package should be installed and included in the 
- * project.
+ * Project Rubric PROJECT ENVIRONMENT SETUP. Success Criteria - Project Dependencies: The body-parser package should be installed 
+ * and included in the project.
  */
 const bodyParser = require('body-parser');
 
 // Start up an instance of app
 /**
- * Project Rubric Success Criteria - Node and Express Environment: Node and Express should be installed on the local machine. 
+ * Project Rubric PROJECT ENVIRONMENT SETUP. Success Criteria - Node and Express Environment: Node and Express should be 
+ * installed on the local machine. 
  * The project file server.js should require express(), and should create an instance of their app using express.
  */
 const app = express();
@@ -28,16 +37,17 @@ const app = express();
 /* Middleware */
 // Here we are configuring express to use body-parser as middleware.
 /**
- * Project Rubric Success Criteria - Project Dependencies: The body-parser package should be installed and included in the 
- * project.
+ * Project Rubric PROJECT ENVIRONMENT SETUP. Success Criteria - Project Dependencies: The body-parser package should be installed 
+ * and included in the project.
  */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross-origin allowance
 /**
- * Project Rubric Success Criteria - Project Dependencies: The ‘cors’ package should be installed in the project from the command
- * line, required in the project file server.js, and the instance of the app should be setup to use cors().
+ * Project Rubric PROJECT ENVIRONMENT SETUP. Success Criteria - Project Dependencies: The ‘cors’ package should be installed in 
+ * the project from the command line, required in the project file server.js, and the instance of the app should be setup to use 
+ * cors().
  */
 app.use(cors());
 
@@ -45,6 +55,12 @@ app.use(cors());
 app.use(express.static('website'));
 
 // GET route to return projectData
+
+/**
+ * Project Rubric APIs AND ROUTES. Success Criteria - Return Endpoint Data - GET Route I: Server Side: There should be a GET 
+ * route setup on the server side with the first argument as a string naming the route, and the second argument a callback 
+ * function to return the JS object created at the top of server code.
+ */
 app.get('/data', (req, res) => {
   res.send(projectData);
 });
@@ -62,8 +78,8 @@ app.post('/data', (req, res) => {
 
 // Setup Server
 /**
- * Project Rubric Success Criteria - Local Server: Local server should be running and producing feedback to the Command Line 
- * through a working callback function.
+ * Project Rubric PROJECT ENVIRONMENT SETUP. Success Criteria - Local Server: Local server should be running and producing 
+ * feedback to the Command Line through a working callback function.
  */
 const port = 3000;
 app.listen(port, () => {
--- website/app.js
+++ website/app.js
@@ -1,11 +1,21 @@
 /* Global Variables */
 /**
- * Project Rubric Success Criteria - API Credentials: Create API credentials on OpenWeatherMap.com
+ * Project Rubric PROJECT ENVIRONMENT SETUP. Success Criteria - API Credentials: Create API credentials on OpenWeatherMap.com
  * Note: The following line of code should be at the top of the app.js file:
  * // Personal API Key for OpenWeatherMap API
  * const apiKey = '<your_api_key>&units=imperial';
  * The actual API key itself will be different for everyone.
  * Notice the last part ‘&units=imperial’-- this should be included in the saved variable.
+ */
+
+/**
+ * Project Rubric APIs AND ROUTES. Success Criteria - Integrating OpenWeatherMap API: The personal API Key for OpenWeatherMap API 
+ * is saved in a named const variable.
+ * The API Key variable is passed as a parameter to fetch(). Data is successfully returned from the external API.
+ * In the file app.js, there should be a line of code near the top:
+ * // Personal API Key for OpenWeatherMap API
+ * const apiKey = '<your_api_key>&units=imperial';
+ * Note: The personal API key must be stored in a const variable.
  */
 const apiKey = 'adb41019c34f749ea04e98d59b8c7561&units=imperial';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
@@ -15,6 +25,11 @@ let d = new Date();
 let newDate = `${d.getMonth() + 1}.${d.getDate()}.${d.getFullYear()}`;
 
 // Async function to get weather data from OpenWeatherMap API
+
+/**
+ * Project Rubric APIs AND ROUTES. Success Criteria - Return Endpoint Data - GET Route II: Client Side: There should be an 
+ * asynchronous function to fetch the data from the app endpoint
+ */
 async function getWeatherData(zipCode) {
   const apiUrl = `${baseUrl}?zip=${zipCode}&appid=${apiKey}`;
   const response = await fetch(apiUrl);
@@ -28,6 +43,12 @@ async function getWeatherData(zipCode) {
   }
 }
 
+/**
+ * Project Rubric DYNAMIC UI. Success Criteria - Event Listeners: Adds an event listener to an existing HTML button from DOM 
+ * using Vanilla JS.
+ * In the file app.js, the element with the id of generate should have an addEventListener() method called on it, with click as 
+ * the first parameter, and a named callback function as the second parameter.
+ */
 document.getElementById('generate').addEventListener('click', performAction);
 
 function performAction() {
@@ -52,6 +73,15 @@ function performAction() {
     });
 }
 
+/**
+ * Project Rubric APIs AND ROUTES. Success Criteria - POST Route: You should be able to add an entry to the project endpoint 
+ * using a POST route setup on the server side and executed on the client side as an asynchronous function.
+ * 
+ * The client side function should take two arguments, the URL to make a POST to, and an object holding the data to POST.
+ * 
+ * The server side function should create a new entry in the apps endpoint (the named JS object) consisting of the data received 
+ * from the client side POST.
+ */
 async function postData(url = '', data = {}) {
   const response = await fetch(url, {
     method: 'POST',
@@ -71,6 +101,13 @@ async function postData(url = '', data = {}) {
     return null;
   }
 }
+
+/**
+ * Project Rubric DYNAMIC UI. Success Criteria - Dynamically Update UI: Sets the properties of existing HTML elements from the 
+ * DOM using Vanilla JavaScript.
+ * Included in the async function to retrieve that app’s data on the client side, existing DOM elements should have their 
+ * innerHTML properties dynamically set according to data returned by the app route.
+ */
 
 async function updateUI() {
   const response = await fetch('/data');
